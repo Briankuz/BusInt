3e285b29b4d4113d21a4179dca2002b7fb89879b
Ro14 <specialneeds_Ro@hotmail.com>
2018-05-25 14:28:54 -0300

    Add files via upload
      Adm Inspector

---
+++ BusInspector.Web/BusInspector.Dal/Models/MetadataInspector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusInspector.Dal
+{
+    public class MetadataInspector
+    {
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        [StringLength(50)]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        [Range(10000000, 99999999, ErrorMessage = "No es correcto")]
+        public Nullable<decimal> dni { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        [Range(1, 100, ErrorMessage = "El numero no es correcto")]
+        public Nullable<decimal> legajo { get; set; }
+    }
+}
---
+++ BusInspector.Web/BusInspector.Dal/Models/PartialInspector.cs
@@ -0,0 +1,12 @@
+﻿using BusInspector.Dal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusInspector.Dal
+{
+    [MetadataType(typeof(MetadataInspector))]
+    public partial class Inspector
+    {
+    }
+}
